8de453b88f01e217decba3245dd26007e9091228
bLadeAyush <prasadom9411@gmail.com>
2025-07-11 10:30:44 +0530

    Added root URL response

--- oj_backend/urls.py
+++ oj_backend/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 
 urlpatterns = [
+    path('', lambda request: HttpResponse("Online Judge Backend Running")),
     path('admin/', admin.site.urls),
     path('api/problems/', include('problems.urls')),     
     path('api/', include('submissions.urls')),   
